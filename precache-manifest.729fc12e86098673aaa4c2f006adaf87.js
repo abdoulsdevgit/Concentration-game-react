4306cb082a0335371cd8211d8042ba309416a334
abdoulsdevgit <abdoulsdevgit@gmail.com>
2021-11-27 23:11:54 -0500

    Updates

---
+++ precache-manifest.729fc12e86098673aaa4c2f006adaf87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0284e60e07c2f999cbee7dd4fa2b6a4c",
+    "url": "/Concentration-game-react/index.html"
+  },
+  {
+    "revision": "87061e58c27b9a58b294",
+    "url": "/Concentration-game-react/static/css/main.1f33bb12.chunk.css"
+  },
+  {
+    "revision": "75bd0151de27c094e166",
+    "url": "/Concentration-game-react/static/js/2.aa400f37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Concentration-game-react/static/js/2.aa400f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87061e58c27b9a58b294",
+    "url": "/Concentration-game-react/static/js/main.65079c06.chunk.js"
+  },
+  {
+    "revision": "3567fd39532847612e47",
+    "url": "/Concentration-game-react/static/js/runtime-main.d256e1b8.js"
+  }
+]);
